9e2f65af40e583dc13a561145fb5b7520612d399
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:30:48 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -3,14 +3,14 @@ from conans import ConanFile, tools, os
 
 class BoostMpiConan(ConanFile):
     name = "Boost.Mpi"
-    version = "1.64.0"
+    version = "1.65.1"
     generators = "boost"
     url = "https://github.com/bincrafters/conan-boost-mpi"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     options = {"shared": [True, False]}
     default_options = "shared=False"
-    requires =  "Boost.Level14Group/1.64.0@bincrafters/testing"
+    requires =  "Boost.Level14Group/1.65.1@bincrafters/testing"
          
     #This library is part of one or more cyclic dependency groups within Boost.
     
