8b01c5ee5966ee450767a516b92bdbc8c3a5cb3d
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:57 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostMpiConan(ConanFile):
     name = "boost_mpi"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_mpi"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["mpi"]
     is_in_cycle_group = True
     is_header_only = False
