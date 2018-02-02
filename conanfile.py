e2d6f0ec97eb1170a1a1e3199cbe57e27910e5b8
Jerry W <jerrywiltse@gmail.com>
2018-02-02 07:08:43 -0500

    Merge testing into stable

--- conanfile.py
+++ conanfile.py
@@ -18,8 +18,8 @@ class BoostMpiConan(ConanFile):
     default_options = "mpicc=default"
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/testing",
-        "boost_level14group/1.66.0@bincrafters/testing"
+        "boost_package_tools/1.66.0@bincrafters/stable",
+        "boost_level14group/1.66.0@bincrafters/stable"
     )
 
     def configure(self):
@@ -38,7 +38,7 @@ class BoostMpiConan(ConanFile):
     short_paths = True
     generators = "boost"
     settings = "os", "arch", "compiler", "build_type"
-    build_requires = "boost_generator/1.66.0@bincrafters/testing"
+    build_requires = "boost_generator/1.66.0@bincrafters/stable"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
