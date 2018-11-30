38fa1d18dbc65a9842fe67d806b224d8bda7704a
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,17 +4,14 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostMpiConan(base.BoostBaseConan):
     name = "boost_mpi"
     url = "https://github.com/bincrafters/conan-boost_mpi"
     lib_short_names = ["mpi"]
-    cycle_group = "boost_level14group"
-    b2_requires = [
-        "boost_level14group",
-    ]
+    cycle_group = "boost_cycle_group_d"
+    b2_requires = ["boost_cycle_group_d"]
 
     def package_info_additional(self):
         self.cpp_info.libs = list(set(self.cpp_info.libs) - set(["mpi"]))
-
